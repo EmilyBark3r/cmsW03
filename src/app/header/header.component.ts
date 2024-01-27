6865b985dfb361f83a8069c13726245a9c460739
BarkerEmily <emilybarker@byui.edu>
2024-01-27 17:38:25 -0500

    where is documents?

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,9 +5,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
