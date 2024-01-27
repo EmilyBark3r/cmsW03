f197412063f2b99da7300fd4c40150cd32989473
BarkerEmily <emilybarker@byui.edu>
2024-01-27 17:28:25 -0500

    working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
