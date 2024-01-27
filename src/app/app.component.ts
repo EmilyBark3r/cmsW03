48fb616ffb1a257ab42dac077cfe49083bc69f38
BarkerEmily <emilybarker@byui.edu>
2024-01-27 17:46:19 -0500

    contacts is working. fix messages and documents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContactsComponent],
+  imports: [RouterOutlet, HeaderComponent, ContactsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
   export class AppComponent {
     title = 'cms';
+
+    selectedFeature!: string;
+
+    switchView(selectedFeature: string) {
+      if (this.selectedFeature?.toUpperCase() !== selectedFeature.toUpperCase()) {
+        this.selectedFeature = selectedFeature;
+      }
+    }
   }
