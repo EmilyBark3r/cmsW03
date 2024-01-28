e861b8158a13ba59e3f70a12a55645cd7e5c2417
BarkerEmily <emilybarker@byui.edu>
2024-01-27 19:08:31 -0500

    messages is looking good

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MessageItemComponent } from './message-item/message-item.component';
+import { MessageEditComponent } from './message-edit/message-edit.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [MessageItemComponent, MessageEditComponent, MessageListComponent],
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
