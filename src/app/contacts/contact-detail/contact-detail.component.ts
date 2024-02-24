c9b811a39c9f8de9613a1a64930fe8c113b1caff
BarkerEmily <emilybarker@byui.edu>
2024-02-24 11:34:57 -0500

    @Input() contact!: Contact;

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -10,8 +10,8 @@ import { ContactsService } from '../contacts.service';
   styleUrl: './contact-detail.component.css'
 })
 export class ContactDetailComponent {
-  // @Input() contact!: Contact;
-  contact!: Contact;
+  @Input() contact!: Contact;
+  // contact!: Contact;
 
   constructor(
     private contactService: ContactsService,
@@ -21,36 +21,3 @@ export class ContactDetailComponent {
     this.contactService.deleteContact(this.contact);
   }
 }
-// import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Params, Router } from '@angular/router';
-// import { Contact } from '../contact.model';
-// import { ContactsService } from '../contacts.service';
-
-// @Component({
-//     selector: 'app-contact-detail',
-//     standalone: true,
-//     imports: [ContactsService],
-//     templateUrl: './contact-detail.component.html',
-//     styleUrl: './contact-detail.component.css'
-//   })
-
-// export class ContactDetailComponent implements OnInit {
-//   contact!: Contact;
-
-//   constructor(
-//     private contactService: ContactService,
-//     private router: Router,
-//     private route: ActivatedRoute
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.route.params.subscribe((params: Params) => {
-//       this.contact = this.contactService.getContact(params['id']);
-//     });
-//   }
-
-//   onDelete() {
-//     this.contactService.deleteContact(this.contact);
-//     this.router.navigate(['../'], { relativeTo: this.route });
-//   }
-// }
