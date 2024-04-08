1c6c7ad9a50194f8f2b67a554cd114ec167d8471
BarkerEmily <emilybarker@byui.edu>
2024-04-08 19:15:28 -0400

    import { Injector, NgModule } from '@angular/core'; import { BrowserModule } from '@angular/platform-browser'; import { CommonModule } from '@angular/common';
    import { AppComponent } from './app.component';
    import { HeaderComponent } from './header.component';
    import { ContactsComponent } from './contacts/contacts.component';
    import { ContactListComponent } from './contacts/contact-list/contact-list.component';
    import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
    import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
    import { DocumentsComponent } from './documents/documents.component';
    import { DocumentListComponent } from './documents/document-list/document-list.component';
    import { DocumentItemComponent } from './documents/document-item/document-item.component';
    import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
    import { MessageItemComponent } from './messages/message-item/message-item.component';
    import { MessageEditComponent } from './messages/message-edit/message-edit.component';
    import { MessageListComponent } from './messages/message-list/message-list.component';
    import { AppRoutingModule } from './app-routing.module';
    import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
    import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
    import { FormsModule } from '@angular/forms';
    import { DndModule } from 'ng2-dnd';
    import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
    
    @NgModule({
      declarations: [
        AppComponent,
        ContactsComponent,
        ContactListComponent,
        ContactDetailComponent,
        ContactItemComponent,
        DocumentsComponent,
        DocumentListComponent,
        DocumentItemComponent,
        DocumentDetailComponent,
        DocumentEditComponent,
        ContactEditComponent,
        HeaderComponent,
        ContactsFilterPipe
      ],
      imports: [CommonModule, BrowserModule, FormsModule, MessageListComponent, AppRoutingModule, MessageItemComponent, MessageEditComponent, DndModule.forRoot()],
      providers: [],
      bootstrap: [AppComponent],
    })
    export class AppModule { }

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,56 @@
-import { Injector, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DndModule } from 'ng2-dnd';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
+import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
 import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentItemComponent } from './documents/document-item/document-item.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { MessageItemComponent } from './messages/message-item/message-item.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
-import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
-import { FormsModule } from '@angular/forms';
-import { DndModule } from 'ng2-dnd';
-import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     ContactsComponent,
     ContactListComponent,
     ContactDetailComponent,
     ContactItemComponent,
+    ContactEditComponent,
+    ContactsFilterPipe,
     DocumentsComponent,
     DocumentListComponent,
     DocumentItemComponent,
     DocumentDetailComponent,
     DocumentEditComponent,
-    ContactEditComponent,
-    HeaderComponent,
-    ContactsFilterPipe
   ],
-  imports: [CommonModule, BrowserModule, FormsModule, MessageListComponent, AppRoutingModule, MessageItemComponent, MessageEditComponent, DndModule.forRoot()],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    DndModule.forRoot(),
+    AppRoutingModule,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
